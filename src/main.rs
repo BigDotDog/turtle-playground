3fb805d7bd606aca078b5aba58f3901d476b2591
BigDotDog <204572233+BigDotDog@users.noreply.github.com>
2025-03-26 21:16:53 +0000

    Use Tortue

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,53 @@
 use std::io;
 
-use turtle::Turtle;
+use macroquad::color::BLUE;
+use tortue::prelude::*;
 
-fn main() {
-    let mut turtle = Turtle::new();
-    turtle.set_speed(25);
+#[macroquad::main("Tortue")]
+async fn main() {
+    let mut turtle = Tortue::new();
 
-    println!("What shape would you like to draw?");
-    println!("Type 1 for a square");
-    println!("Type 2 for a triangle");
-    println!("Type 3 for a circle");
-    print!("Type here: ");
+    loop {
+        println!("What shape would you like to draw?");
+        println!("Type 1 for a square");
+        println!("Type 2 for a triangle");
+        println!("Type 3 for a circle");
+        print!("Type here: ");
 
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    let shape: u32 = input.trim().parse().unwrap_or(1);
-    match shape {
-        1 => {
-            for _ in 0..4 {
-                turtle.forward(100.0);
-                turtle.right(90.0);
+        let shape: u32 = input.trim().parse().unwrap_or(1);
+        match shape {
+            1 => {
+                for _ in 0..4 {
+                    turtle.forward(100.0);
+                    turtle.right(90.0);
+                }
+            }
+            2 => {
+                for _ in 0..3 {
+                    turtle.forward(100.0);
+                    turtle.right(120.0);
+                }
+            }
+            3 => {
+                for _ in 0..360 {
+                    turtle.forward(2.0);
+                    turtle.right(1.0);
+                }
+            }
+            _ => {
+                turtle.set_pen_color(BLUE);
+                for _ in 0..8 {
+                    turtle.forward(50.0);
+                    turtle.right(45.0);
+                }
             }
         }
-        2 => {
-            for _ in 0..3 {
-                turtle.forward(100.0);
-                turtle.right(120.0);
-            }
-        }
-        3 => {
-            for _ in 0..3 {
-                turtle.forward(100.0);
-                turtle.right(120.0);
-            }
-        }
-        _ => {
-            turtle.set_pen_color("purple");
-            for _ in 0..8 {
-                turtle.forward(50.0);
-                turtle.right(45.0);
-            }
-        }
+
+        turtle.draw().await;
     }
 }
